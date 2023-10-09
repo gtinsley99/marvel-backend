1d1fc467e94905498456f4a945cd44187df8ea7e
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-10-09 10:01:30 +0100

    add user model

---
+++ src/Users/model.js
@@ -0,0 +1,24 @@
+const {DataTypes} = require("sequelize");
+const connection = require("../db/connection");
+
+const User = connection.define("User", {
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        validate: {
+            is: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        },
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;

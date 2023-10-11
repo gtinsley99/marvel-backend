2ed324eeaecc234a38afd24fc0171adb5f708368
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-10-11 11:25:06 +0100

    add routes to find one char and all char working

--- src/Characters/controllers.js
+++ src/Characters/controllers.js
@@ -5,15 +5,45 @@ const addCharacter = async (req, res) => {
         const char = await Character.create({
             name: req.body.name,
             image: req.body.image,
-            description: req.body.description,
             marvelID: req.body.marvelID,
         })
         res.status(200).json({
             message: "success",
             name: char.name,
             image: char.image,
-            description: char.description,
             marvelID: char.marvelID
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(501).json({
+          message: error.message,
+          detail: error,
+        });
+    }
+};
+
+const getAll = async (req, res) => {
+    try {
+        const char = await Character.findAll({});
+        res.status(200).json({
+            message: "success",
+            characters: char
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(501).json({
+          message: error.message,
+          detail: error,
+        });
+    }
+};
+
+const getOne = async (req, res) => {
+    try {
+        const char = await Character.findOne({where: {name: req.params.name}});
+        res.status(200).json({
+            message: "success",
+            character: char
         });
     } catch (error) {
         console.log(error);
@@ -26,4 +56,6 @@ const addCharacter = async (req, res) => {
 
 module.exports = {
     addCharacter,
+    getAll,
+    getOne
 }
